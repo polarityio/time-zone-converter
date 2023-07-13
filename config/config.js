58585c36254331d23e96c2fefdb4d8be003e3029
CJ-Polarity <127783370+CJ-Polarity@users.noreply.github.com>
2023-07-13 10:00:48 -0400

    chore: Set logging to info

--- config/config.js
+++ config/config.js
@@ -46,7 +46,7 @@ module.exports = {
     proxy: ''
   },
   logging: {
-    level: 'trace' //trace, debug, info, warn, error, fatal
+    level: 'info' //trace, debug, info, warn, error, fatal
   },
   options: [
     {
