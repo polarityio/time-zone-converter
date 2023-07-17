7aedd13682f071eeccd3da08d5f8ebdddc389c08
Jon Penwood <jonp@polarity.io>
2023-07-17 11:33:53 -0500

    fix: Using variable

--- src/parsers/parseRFC3339.js
+++ src/parsers/parseRFC3339.js
@@ -2,19 +2,15 @@ const { DateTime } = require('luxon');
 const { checkSecondsRegex, getTimeZonesForThisDateTime } = require('./utils');
 
 const parseRFC3339 = (value, options, isDetailed) => {
-  let formattedTime;
-  let timezone;
-
   const valueWithoutZ = value.endsWith('Z')
     ? value.substring(0, value.length - 1)
     : value;
-  if (value.endsWith('Z')) {
-    value = value.substring(0, value.length - 1);
-  }
 
-  const formatString = `yyyy-MM-dd HH:mm${checkSecondsRegex.test(value) ? ':ss' : ''}`;
+  const formatString = `yyyy-MM-dd HH:mm${
+    checkSecondsRegex.test(valueWithoutZ) ? ':ss' : ''
+  }`;
 
-  formattedTime = DateTime.fromFormat(value, formatString, { zone: 'utc' });
+  const formattedTime = DateTime.fromFormat(valueWithoutZ, formatString, { zone: 'utc' });
 
   return getTimeZonesForThisDateTime(formattedTime, options, isDetailed);
 };
