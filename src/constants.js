790b762726f79928eb9b6357b0da82751ac3ca26
CJ-Polarity <127783370+CJ-Polarity@users.noreply.github.com>
2023-07-26 11:10:51 -0400

    fix: Changed options for summary and details to Daylight time

--- src/constants.js
+++ src/constants.js
@@ -2,27 +2,62 @@ const TIMEZONES = [
   { name: 'International Date Line West', abbreviation: 'IDLW', value: 'Etc/GMT+12' },
   { name: 'Nome Time', abbreviation: 'NT', value: 'America/Nome' },
   {
-    name: 'Hawaii-Aleutian Standard Time',
-    abbreviation: 'HST',
+    name: 'Hawaii-Aleutian Standard Time (HAST/HADT)',
+    abbreviation: 'HAST',
     value: 'Pacific/Honolulu'
   },
-  { name: 'Alaska Standard Time', abbreviation: 'AKST', value: 'America/Anchorage' },
-  { name: 'Pacific Standard Time', abbreviation: 'PST', value: 'America/Los_Angeles' },
-  { name: 'Mountain Standard Time', abbreviation: 'MST', value: 'America/Denver' },
-  { name: 'Central Standard Time', abbreviation: 'CST', value: 'America/Chicago' },
-  { name: 'Eastern Standard Time', abbreviation: 'EST', value: 'America/New_York' },
-  { name: 'Atlantic Standard Time', abbreviation: 'AST', value: 'America/Halifax' },
-  { name: 'Newfoundland Standard Time', abbreviation: 'NST', value: 'America/St_Johns' },
+  {
+    name: 'Alaska Standard Time (AKST/AKDT)',
+    abbreviation: 'AKST',
+    value: 'America/Anchorage'
+  },
+  {
+    name: 'Pacific Standard Time (PST/PDT)',
+    abbreviation: 'PST',
+    value: 'America/Los_Angeles'
+  },
+  {
+    name: 'Mountain Standard Time (MST/MDT)',
+    abbreviation: 'MST',
+    value: 'America/Denver'
+  },
+  {
+    name: 'Central Standard Time (CST/CDT)',
+    abbreviation: 'CST',
+    value: 'America/Chicago'
+  },
+  {
+    name: 'Eastern Standard Time (EST/EDT)',
+    abbreviation: 'EST',
+    value: 'America/New_York'
+  },
+  {
+    name: 'Atlantic Standard Time (AST/ADT)',
+    abbreviation: 'AST',
+    value: 'America/Halifax'
+  },
+  {
+    name: 'Newfoundland Standard Time (NST/NDT)',
+    abbreviation: 'NST',
+    value: 'America/St_Johns'
+  },
   { name: 'Greenwich Mean Time', abbreviation: 'GMT', value: 'Etc/GMT' },
-  { name: 'Central European Time', abbreviation: 'CET', value: 'Europe/Paris' },
-  { name: 'Eastern European Time', abbreviation: 'EET', value: 'Europe/Athens' },
-  { name: 'Moscow Standard Time', abbreviation: 'MSK', value: 'Europe/Moscow' },
+  {
+    name: 'Central European Time (CET/CEDT)',
+    abbreviation: 'CET',
+    value: 'Europe/Paris'
+  },
+  {
+    name: 'Eastern European Time (EET/EEDT)',
+    abbreviation: 'EET',
+    value: 'Europe/Athens'
+  },
   { name: 'India Standard Time', abbreviation: 'IST', value: 'Asia/Kolkata' },
   { name: 'Western Indonesian Time', abbreviation: 'WIB', value: 'Asia/Jakarta' },
   { name: 'Hong Kong Time', abbreviation: 'HKT', value: 'Asia/Hong_Kong' },
   { name: 'Japan Standard Time', abbreviation: 'JST', value: 'Asia/Tokyo' },
   {
-    name: 'Australian Eastern Standard Time',
+    name: 'Australian Eastern Standard Time (AEST/AEDT)',
     abbreviation: 'AEST',
     value: 'Australia/Sydney'
   },
